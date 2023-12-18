1f14570f4665b32ab880a6fc724fc90c6cf97165
endlo <endlone000@gmail.com>
2023-12-18 02:58:51 +0200

    Added Capsule Logic

--- src/main/java/net/backupcup/everlasting/mixin/ItemStackMixin.java
+++ src/main/java/net/backupcup/everlasting/mixin/ItemStackMixin.java
@@ -2,25 +2,40 @@ package net.backupcup.everlasting.mixin;
 
 import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import net.backupcup.everlasting.assign.RegisterEffects;
+import net.backupcup.everlasting.assign.RegisterItems;
 import net.backupcup.everlasting.config.configHandler;
 import net.backupcup.everlasting.enchantment.RestoringEnchantment;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.random.Random;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
+
+    @Shadow public abstract void setNbt(@Nullable NbtCompound nbt);
+
     @WrapWithCondition(method = "damage(ILnet/minecraft/util/math/random/Random;Lnet/minecraft/server/network/ServerPlayerEntity;)Z",
                        at = @At(value = "INVOKE",
                        target = "Lnet/minecraft/item/ItemStack;setDamage(I)V"))
     private boolean shouldPreventDamage(ItemStack stack, int amount, int damage, Random random, ServerPlayerEntity player) {
-        if(!configHandler.INSTANCE.containsItemID(stack.getItem().getTranslationKey().trim())) {
+        if(!configHandler.INSTANCE.containsItemID(stack.getItem().getTranslationKey().trim()) && stack.getItem() != RegisterItems.INSTANCE.getCAPSULE()) {
             if(player != null) {
                 if (player.hasStatusEffect(RegisterEffects.INSTANCE.getEVERLASTING())) {
                     return false;
                 }
+
+                for(ItemStack inventoryStack : player.getInventory().main) {
+                    if(inventoryStack.getItem() == RegisterItems.INSTANCE.getCAPSULE() && (inventoryStack.getMaxDamage() - inventoryStack.getDamage()) > damage) {
+                        inventoryStack.damage(damage, random, player);
+                        return false;
+                    }
+                }
+
                 if (RestoringEnchantment.Companion.shouldPreventDamage(stack, damage, player)) {
                     player.addExperience(-damage);
                     return false;
