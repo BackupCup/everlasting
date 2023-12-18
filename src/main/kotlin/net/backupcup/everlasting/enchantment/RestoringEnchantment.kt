007d48ee1ddd7d028bf9b860af6014768b7cfa1b
endlo <endlone000@gmail.com>
2023-12-18 02:58:37 +0200

    Made Restoring incompatible with the Capsule

--- src/main/kotlin/net/backupcup/everlasting/enchantment/RestoringEnchantment.kt
+++ src/main/kotlin/net/backupcup/everlasting/enchantment/RestoringEnchantment.kt
@@ -1,6 +1,9 @@
 package net.backupcup.everlasting.enchantment
 
+import net.backupcup.everlasting.Everlasting
 import net.backupcup.everlasting.Everlasting.restoringSlots
+import net.backupcup.everlasting.assign.RegisterItems
+import net.backupcup.everlasting.items.CapsuleItem
 import net.minecraft.enchantment.Enchantment
 import net.minecraft.enchantment.EnchantmentHelper
 import net.minecraft.enchantment.EnchantmentTarget
@@ -17,21 +20,17 @@ class RestoringEnchantment(weight: Rarity?, target: EnchantmentTarget?, slotType
         return super.canAccept(other)
     }
 
+    override fun isAcceptableItem(stack: ItemStack?): Boolean {
+        return if (stack!!.isOf(RegisterItems.CAPSULE)) false
+        else super.isAcceptableItem(stack)
+    }
+
     override fun isTreasure(): Boolean {
         return true
     }
 
     override fun isAvailableForEnchantedBookOffer(): Boolean {
         return false
-    }
-
-    companion object {
-        @JvmField
-        val INSTANCE: RestoringEnchantment = RestoringEnchantment(Rarity.COMMON, EnchantmentTarget.BREAKABLE, restoringSlots)
-
-        fun shouldPreventDamage(itemStack: ItemStack, amount: Int, player: ServerPlayerEntity): Boolean {
-            return EnchantmentHelper.getLevel(INSTANCE, itemStack) > 0 && player.totalExperience >= amount
-        }
     }
 
     override fun getMaxLevel(): Int {
@@ -44,5 +43,14 @@ class RestoringEnchantment(weight: Rarity?, target: EnchantmentTarget?, slotType
 
     override fun getMaxPower(level: Int): Int {
         return getMinPower(level) + 60
+    }
+
+    companion object {
+        @JvmField
+        val INSTANCE: RestoringEnchantment = RestoringEnchantment(Rarity.COMMON, EnchantmentTarget.BREAKABLE, restoringSlots)
+
+        fun shouldPreventDamage(itemStack: ItemStack, amount: Int, player: ServerPlayerEntity): Boolean {
+            return EnchantmentHelper.getLevel(INSTANCE, itemStack) > 0 && player.totalExperience >= amount
+        }
     }
 }
