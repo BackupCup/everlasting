a869a600acffdd22987c97f65bac5c95a6dc3d02
endlo <endlone000@gmail.com>
2024-01-21 02:54:36 +0200

    Optimized imports

--- src/main/kotlin/net/backupcup/everlasting/items/CapsuleItem.kt
+++ src/main/kotlin/net/backupcup/everlasting/items/CapsuleItem.kt
@@ -4,7 +4,6 @@ import net.minecraft.client.item.TooltipContext
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
-import net.minecraft.resource.featuretoggle.FeatureSet
 import net.minecraft.text.Text
 import net.minecraft.util.Formatting
 import net.minecraft.world.World
